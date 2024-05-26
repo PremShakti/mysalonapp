1885e390855a0d51501a103a2c3f8b13c9ac72dc
PremShakti <premshakti.ps@gmail.com>
2024-05-26 21:45:24 +0530

    a

--- src/data/slider.js
+++ src/data/slider.js
@@ -1,22 +1,22 @@
 export const Sdata =[
     {
-      src: "https://framerusercontent.com/images/OEhnOJLjxZJLueiEGMYs9oASY4.png",
+      src: "https://as1.ftcdn.net/v2/jpg/04/23/25/24/1000_F_423252426_FdTfSBTeMUJmfOgHmBuM2YaIWqUYVYRb.jpg",
       srcM: "https://images.pexels.com/photos/7755461/pexels-photo-7755461.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
-      src: "https://framerusercontent.com/images/OEhnOJLjxZJLueiEGMYs9oASY4.png",
+      src: "https://as2.ftcdn.net/v2/jpg/04/83/71/95/1000_F_483719567_tRNWJWCE2H2nKAEqEQjStfhBbqrfBkaJ.jpg",
       srcM: "https://images.pexels.com/photos/10318038/pexels-photo-10318038.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
-      src: "https://framerusercontent.com/images/OEhnOJLjxZJLueiEGMYs9oASY4.png",
+      src: "https://as1.ftcdn.net/v2/jpg/05/01/36/50/1000_F_501365094_Hx0zmIgFOZUMJvnzHPqwUuORZouN5T5Y.jpg",
       srcM: "https://images.pexels.com/photos/10318038/pexels-photo-10318038.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
-      src: "https://framerusercontent.com/images/OEhnOJLjxZJLueiEGMYs9oASY4.png",
+      src: "https://as1.ftcdn.net/v2/jpg/01/75/44/62/1000_F_175446296_DTeiaIrSVTMckmotpt38aon6Yj4U0Fl4.jpg",
       srcM: "https://images.pexels.com/photos/10318038/pexels-photo-10318038.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
-      src: "https://framerusercontent.com/images/OEhnOJLjxZJLueiEGMYs9oASY4.png",
+      src: "https://as1.ftcdn.net/v2/jpg/06/48/53/82/1000_F_648538289_PzWYhuDVcf0iXSDpSB01E9NeeeQDqhBG.jpg",
       srcM: "https://images.pexels.com/photos/10318038/pexels-photo-10318038.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
   ];
